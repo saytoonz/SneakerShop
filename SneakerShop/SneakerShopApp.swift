77008f8733b7e7e0a3c209383065fd244529e0d8
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2023-02-23 23:07:51 +0000

    Initial Commit

---
+++ SneakerShop/SneakerShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SneakerShopApp.swift
+//  SneakerShop
+//
+//  Created by Sam on 23/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SneakerShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
